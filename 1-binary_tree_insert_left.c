3201708b57f639a4c706cb1c2996d3b770d5f033
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-09-20 16:20:08 +0000

    Add task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,21 +16,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	tmp = parent->left;
+	new_node->parent = parent;
 	if (tmp == NULL)
 	{
 		tmp = new_node;
 		new_node->left = NULL;
-		new_node->parent = parent;
 	}
 	else
 	{
-		new_node->parent = parent;
 		new_node->left = tmp;
 		tmp->parent = new_node;
-		parent->left = new_node;
-
 	}
 
+	parent->left = new_node;
 	new_node->right = NULL;
 	new_node->n = value;
 	return (new_node);
