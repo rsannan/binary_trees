746966b1ed25b1ae54f196ec533d8c52cfbcc26e
Reginald Shawn Annan <shawnrannan@gmail.com>
2022-09-20 18:52:59 +0000

    Add task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_delete - deletes a binary tree
+* @tree: pointer to root
+*
+* Return: Void
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *node = tree;
+	
+	if (node != NULL)
+	{
+		binary_tree_delete(node->left);
+		binary_tree_delete(node->right);
+		free(node);
+	}
+
+}
